690d54185698af716afaf35a64498b1090c5e7a9
Serj Sidelnikov <sv@spiks.ru>
2020-09-18 12:47:23 +0300

    Wrote a script AccentTypographyBuild

---
+++ source/js/modules/accentTypography.js
@@ -0,0 +1,63 @@
+export default class AccentTypographyBuild {
+  constructor(elementSelector, timer, classForActive, property) {
+    this._TIME_SPACE = 100;
+
+    this._element = document.querySelector(elementSelector);
+    this._timer = timer;
+    this._classForActive = classForActive;
+    this._property = property;
+    this._timeOffset = 0;
+    this._step = 20;
+
+    this.prePareText();
+  }
+
+  createElement(letter) {
+    const span = document.createElement(`span`);
+    span.textContent = letter;
+    span.style.transition = `${this._property} ${this._timer}ms ease ${this._timeOffset}ms`;
+    this._timeOffset += this._step;
+    return span;
+  }
+
+  prePareText() {
+    if (!this._element) {
+      return;
+    }
+
+    const text = this._element.textContent
+      .trim()
+      .split(` `)
+      .filter((el) => el !== ``);
+
+    const content = text.reduce((fragmentParent, word) => {
+      const wordElement = Array.from(word)
+        .reduce((fragment, letter) => {
+          fragment.appendChild(this.createElement(letter));
+          return fragment;
+        }, document.createDocumentFragment());
+
+      const wordContainer = document.createElement(`span`);
+      wordContainer.classList.add(`text__word`);
+      wordContainer.appendChild(wordElement);
+      fragmentParent.appendChild(wordContainer);
+
+      return fragmentParent;
+    }, document.createDocumentFragment());
+
+    this._element.innerHTML = ``;
+    this._element.appendChild(content);
+  }
+
+  runAnimation() {
+    if (!this._element) {
+      return;
+    }
+
+    this._element.classList.add(this._classForActive);
+  }
+
+  destroyAnimation() {
+    this._element.classList.remove(this._classForActive);
+  }
+}
