6b6455d26b493ee0ea792b3eac07f5df17448ea5
Serj Sidelnikov <sv@spiks.ru>
2020-09-18 14:21:54 +0300

    Animate intro__date

--- source/js/modules/animateText.js
+++ source/js/modules/animateText.js
@@ -4,6 +4,8 @@ export default () => {
   const screenIntro = document.querySelector(`.screen--intro`);
   const animateIntroTitle = new AccentTypographyBuild(`.intro__title`, `500`, `active`, `transform`);
 
+  const animateIntroDate = new AccentTypographyBuild(`.intro__date`, `350`, `active`, `transform`);
+
   document.body.addEventListener(`screenChanged`, () => {
     const screenIntroActive = screenIntro.classList.contains(`active`);
 
@@ -11,8 +13,12 @@ export default () => {
       setTimeout(() => {
         animateIntroTitle.runAnimation();
       }, 350);
+
+      setTimeout(() => {
+        animateIntroDate.runAnimation();
+      }, 1000);
     } else {
-      animateIntroTitle.destroyAnimation();
+      animateIntroDate.destroyAnimation();
     }
   });
 };
